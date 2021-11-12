4f8449d7a7a8ee1b5afa5090c38e943affce1675
keys4words <keys4words@gmail.com>
2021-11-12 16:25:13 +0300

    add scripts to ansible-lab

---
+++ ansible-lab/app.sh
@@ -0,0 +1,13 @@
+
+# Configure Django on app server.
+ansible app -b -m yum -a "name=python3-pip state=present"
+ansible app -b -m pip -a "name=django<4 state=present"
+
+# Check Django version.
+ansible app -a "python3 -m django --version"
+
+# Other commands from the book.
+# ansible app -b -m group -a "name=admin state=present"
+# ansible app -b -m user -a "name=johndoe group=admin createhome=yes"
+# ansible app -b -m user -a "name=johndoe state=absent remove=yes"
+# ansible app -b -m package -a "name=git state=present"
